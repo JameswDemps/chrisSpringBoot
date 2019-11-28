7f456a4959fd58ee424aa11681904000ed9b2ba5
james <jameswdemps@gmail.com>
2019-11-28 16:26:19 +0000

    Token Config java doc

--- src/main/java/com/nationwide/config/TokenConfiguration.java
+++ src/main/java/com/nationwide/config/TokenConfiguration.java
@@ -8,10 +8,22 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+/**
+ * Integrates swagger 2 into the project.
+ * 
+ * Defines the Docket bean and finds a way to control the endpoints exposed by Swagger.
+ * 
+ * Predicates for selection of RequestHandlers can be configured
+ *  with the help of RequestHandlerSelectors and PathSelectors. Using
+ *   any() for both will make documentation for your entire API available through Swagger.
+ * 
+ * @author n/a
+ *
+ */
 @Configuration
 @EnableSwagger2
 public class TokenConfiguration {
+	
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
@@ -20,4 +32,5 @@ public class TokenConfiguration {
 				.paths(PathSelectors.any())
 				.build();
 	}
+	
 }
